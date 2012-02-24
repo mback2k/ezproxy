95e3c112705a58719b854c0e35db1dd25957d90e
Marc Hoersken <info@marc-hoersken.de>
2012-02-24 13:33:44 +0100

    Added login and logout handling

--- src/de/uxnr/ezproxy/EZProxyRewriter.java
+++ src/de/uxnr/ezproxy/EZProxyRewriter.java
@@ -1,31 +1,38 @@
 package de.uxnr.ezproxy;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map.Entry;
 
 import de.uxnr.proxy.Headers;
 import de.uxnr.proxy.HostRewriter;
 
 public class EZProxyRewriter implements HostRewriter {
 	private static EZProxySession session;
-	
+
 	public static void setSession(EZProxySession session) {
 		EZProxyRewriter.session = session;
 	}
-	
+
 	@Override
 	public void rewriteRequest(StringBuilder requestMethod,
 			StringBuilder requestURI, Headers requestHeaders)
 			throws IOException {
 
 		if (EZProxyRewriter.session != null) {
-			int offset = requestURI.indexOf("/", requestURI.indexOf("://")+3);
-			requestURI.insert(offset, ".ezproxy.dhbw-mannheim.de");
-			requestHeaders.add("Cookie", session.getCookie());
+			String url = requestURI.toString();
+			String domain = session.getDomain();
+			String cookie = session.getCookie();
+			if (!url.contains(domain)) {
+				int offset = requestURI.indexOf("/",
+						requestURI.indexOf("://") + 3);
+				requestURI.insert(offset, "." + domain);
+			}
+			if (!cookie.isEmpty()) {
+				requestHeaders.add("Cookie", cookie);
+			}
 		}
 
 		System.out.println(requestURI.toString());
+		System.out.println(">>> " + requestHeaders.entrySet());
 	}
 
 	@Override
@@ -33,14 +40,6 @@ public class EZProxyRewriter implements HostRewriter {
 			StringBuilder requestURI, Headers requestHeaders,
 			Headers responseHeaders) throws IOException {
 
-		for (Entry<String, List<String>> header : responseHeaders.entrySet()) {
-			StringBuilder print = new StringBuilder(header.getKey());
-			print.append(": ");
-			for (String prop : header.getValue()) {
-				print.append(prop);
-				print.append("; ");
-			}
-			System.out.println(print);
-		}
+		System.out.println("<<< " + responseHeaders.entrySet());
 	}
 }
--- src/de/uxnr/ezproxy/EZProxySession.java
+++ src/de/uxnr/ezproxy/EZProxySession.java
@@ -1,13 +1,144 @@
 package de.uxnr.ezproxy;
 
-import javax.swing.UIManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Properties;
 
 public class EZProxySession {
+	private String cookie = "";
+	private String username;
+	private String password;
+	private String domain;
+	private URL location;
+	private int port;
+
 	protected void launch() throws Exception {
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		load();
+		login();
+	}
+
+	protected void finalize() throws Exception {
+		logout();
+	}
+
+	protected String getDomain() {
+		return this.domain;
 	}
 
 	protected String getCookie() {
-		return "";
+		return this.cookie;
+	}
+
+	protected int getPort() {
+		return this.port;
+	}
+
+	private void load() throws IOException {
+		Properties properties = new Properties();
+
+		File config = new File("ezproxy.cfg");
+		if (!config.exists() || !config.canRead()) {
+			config.createNewFile();
+
+			properties.setProperty("username", "");
+			properties.setProperty("password", "");
+			properties.setProperty("domain", "ezproxy.dhbw-mannheim.de");
+			properties.setProperty("port", "12345");
+			properties.store(new FileOutputStream(config),
+					"EZProxy configuration");
+		} else {
+			properties.load(new FileInputStream(config));
+		}
+
+		this.username = properties.getProperty("username", "");
+		this.password = properties.getProperty("password", "");
+		this.domain = properties.getProperty("domain",
+				"ezproxy.dhbw-mannheim.de");
+		this.port = Integer.parseInt(properties.getProperty("port", "12345"));
+
+		if (this.username.isEmpty() || this.password.isEmpty()
+				|| this.domain.isEmpty()) {
+			throw new Error(
+					"Please set username, password and domain in ezproxy.cfg");
+		}
+	}
+
+	private void login() throws IOException {
+		this.location = new URL("https://" + this.domain + "/login");
+
+		int counter = 0;
+		while (perform()) {
+			if (counter > 3) {
+				throw new Error("Unable to login at EZProxy");
+			} else {
+				counter++;
+			}
+		}
+
+		System.out.println("Successfully logged-in using cookie: "
+				+ this.cookie);
+	}
+
+	private void logout() throws IOException {
+		this.location = new URL("https://" + this.domain + "/logout");
+
+		int counter = 0;
+		while (perform()) {
+			if (counter > 3) {
+				throw new Error("Unable to logout at EZProxy");
+			} else {
+				counter++;
+			}
+		}
+
+		System.out.println("Successfully logged-out");
+	}
+
+	private boolean perform() throws IOException {
+		URLConnection conn = this.location.openConnection();
+
+		HttpURLConnection connection = null;
+		if (conn instanceof URLConnection) {
+			connection = (HttpURLConnection) conn;
+		} else {
+			throw new IOException("Unsupported non-HTTP connection");
+		}
+
+		connection.setRequestMethod("POST");
+		connection.setDefaultUseCaches(false);
+		connection.setInstanceFollowRedirects(false);
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Cookie", this.cookie);
+		connection.connect();
+
+		OutputStreamWriter output = new OutputStreamWriter(
+				connection.getOutputStream());
+		output.write("user=" + URLEncoder.encode(this.username, "utf-8")
+				+ "&pass=" + URLEncoder.encode(this.password, "utf-8")
+				+ "&submit=Login");
+		output.flush();
+		output.close();
+
+		if (connection.getResponseCode() == 302) {
+			String cookie = connection.getHeaderField("Set-Cookie");
+			String location = connection.getHeaderField("Location");
+			if (cookie != null) {
+				this.cookie = cookie.split(";")[0];
+			}
+			if (location != null) {
+				this.location = new URL(location);
+			}
+			return true;
+		}
+
+		return false;
 	}
 }
