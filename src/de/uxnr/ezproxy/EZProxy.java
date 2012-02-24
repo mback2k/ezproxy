75ac85430f2b20c477fce9296b59ef681d50e764
Marc Hoersken <info@marc-hoersken.de>
2012-02-24 13:34:10 +0100

    Added dialogs and systray icon

--- src/de/uxnr/ezproxy/EZProxy.java
+++ src/de/uxnr/ezproxy/EZProxy.java
@@ -1,15 +1,68 @@
 package de.uxnr.ezproxy;
 
+import java.awt.AWTException;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 import de.uxnr.proxy.Proxy;
 
 public class EZProxy {
-	public static void main(String[] args) throws Exception {
-		EZProxySession session = new EZProxySession();
-		EZProxyRewriter.setSession(session);
-		session.launch();
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			// Nothing to do here
+		}
 
-		Proxy proxy = new Proxy(12348);
-		proxy.addHostRewriter(".*", new EZProxyRewriter());
-		proxy.start();
+		try {
+			EZProxySession session = new EZProxySession();
+			EZProxyRewriter.setSession(session);
+			session.launch();
+
+			Proxy proxy = new Proxy(session.getPort());
+			proxy.addHostRewriter(".*", new EZProxyRewriter());
+			proxy.start();
+
+			JOptionPane.showMessageDialog(null,
+					"Proxy is running on localhost:" + session.getPort(),
+					"Proxy", JOptionPane.INFORMATION_MESSAGE);
+			EZProxy.tray(proxy);
+
+			session.finalize();
+		} catch (Error e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Exception",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	private static void tray(final Proxy proxy) throws AWTException {
+		final SystemTray tray = SystemTray.getSystemTray();
+		final ImageIcon icon = new ImageIcon(
+				EZProxy.class.getResource("EZProxy.png"));
+		final TrayIcon trayIcon = new TrayIcon(icon.getImage(), "EZProxy");
+		final MenuItem exitItem = new MenuItem("Exit");
+		final PopupMenu popup = new PopupMenu();
+
+		exitItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				proxy.stop();
+				tray.remove(trayIcon);
+			}
+		});
+
+		popup.add(exitItem);
+		trayIcon.setPopupMenu(popup);
+		tray.add(trayIcon);
 	}
 }
