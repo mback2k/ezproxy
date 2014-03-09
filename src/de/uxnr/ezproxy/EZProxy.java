dbc6746f4d9b41751ea54462bedc183d040feb96
Marc Hoersken <info@marc-hoersken.de>
2014-03-09 12:09:20 +0100

    Applied Google Code Style Guide format

--- src/de/uxnr/ezproxy/EZProxy.java
+++ src/de/uxnr/ezproxy/EZProxy.java
@@ -15,55 +15,57 @@ import javax.swing.UIManager;
 import de.uxnr.proxy.Proxy;
 
 public class EZProxy {
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			// Nothing to do here
-		}
+  public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      // Nothing to do here
+    }
 
-		try {
-			EZProxySession session = new EZProxySession();
-			session.begin();
+    try {
+      EZProxySession session = new EZProxySession();
+      session.begin();
 
-			EZProxyRewriter rewriter = new EZProxyRewriter();
-			rewriter.setSession(session);
+      EZProxyRewriter rewriter = new EZProxyRewriter();
+      rewriter.setSession(session);
 
-			Proxy proxy = new Proxy(session.getPort());
-			proxy.addHostRewriter(".*", rewriter);
-			proxy.start();
+      Proxy proxy = new Proxy(session.getPort());
+      proxy.addHostRewriter(".*", rewriter);
+      proxy.start();
 
-			JOptionPane.showMessageDialog(null, "Proxy is running on localhost:" + session.getPort(), "Proxy", JOptionPane.INFORMATION_MESSAGE);
-			EZProxy.tray(proxy, session);
-		} catch (Error e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+      JOptionPane.showMessageDialog(null, "Proxy is running on localhost:" + session.getPort(),
+          "Proxy", JOptionPane.INFORMATION_MESSAGE);
+      EZProxy.tray(proxy, session);
+    } catch (Error e) {
+      JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
+    }
+  }
 
-	private static void tray(final Proxy proxy, final EZProxySession session) throws AWTException {
-		final SystemTray tray = SystemTray.getSystemTray();
-		final ImageIcon icon = new ImageIcon(EZProxy.class.getResource("EZProxy.png"));
-		final TrayIcon trayIcon = new TrayIcon(icon.getImage(), "EZProxy");
-		final MenuItem exitItem = new MenuItem("Exit");
-		final PopupMenu popup = new PopupMenu();
+  private static void tray(final Proxy proxy, final EZProxySession session) throws AWTException {
+    final SystemTray tray = SystemTray.getSystemTray();
+    final ImageIcon icon = new ImageIcon(EZProxy.class.getResource("EZProxy.png"));
+    final TrayIcon trayIcon = new TrayIcon(icon.getImage(), "EZProxy");
+    final MenuItem exitItem = new MenuItem("Exit");
+    final PopupMenu popup = new PopupMenu();
 
-		exitItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				proxy.stop();
-				try {
-					session.end();
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(null, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
-				}
-				tray.remove(trayIcon);
-				System.exit(0);
-			}
-		});
+    exitItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        proxy.stop();
+        try {
+          session.end();
+        } catch (Exception e) {
+          JOptionPane.showMessageDialog(null, e.getMessage(), "Exception",
+              JOptionPane.ERROR_MESSAGE);
+        }
+        tray.remove(trayIcon);
+        System.exit(0);
+      }
+    });
 
-		popup.add(exitItem);
-		trayIcon.setPopupMenu(popup);
-		tray.add(trayIcon);
-	}
+    popup.add(exitItem);
+    trayIcon.setPopupMenu(popup);
+    tray.add(trayIcon);
+  }
 }
--- src/de/uxnr/ezproxy/EZProxyRewriter.java
+++ src/de/uxnr/ezproxy/EZProxyRewriter.java
@@ -9,48 +9,50 @@ import de.uxnr.proxy.Headers;
 import de.uxnr.proxy.HostRewriter;
 
 public class EZProxyRewriter implements HostRewriter {
-	private static final String CONTENT_TYPE_HEADER_UPPER = "Content-Type";
-	private static final String CONTENT_TYPE_HEADER_LOWER = "Content-type";
-	private static final String COOKIE_HEADER = "Cookie";
+  private static final String CONTENT_TYPE_HEADER_UPPER = "Content-Type";
+  private static final String CONTENT_TYPE_HEADER_LOWER = "Content-type";
+  private static final String COOKIE_HEADER = "Cookie";
 
-	private EZProxySession session;
+  private EZProxySession session;
 
-	public synchronized void setSession(EZProxySession session) {
-		this.session = session;
-	}
+  public synchronized void setSession(EZProxySession session) {
+    this.session = session;
+  }
 
-	@Override
-	public synchronized void rewriteRequest(StringBuilder requestMethod, StringBuilder requestURI, Headers requestHeaders) throws IOException {
+  @Override
+  public synchronized void rewriteRequest(StringBuilder requestMethod, StringBuilder requestURI,
+      Headers requestHeaders) throws IOException {
 
-		if (this.session != null) {
-			String url = requestURI.toString();
-			String domain = this.session.getDomain();
-			String cookie = this.session.getCookie();
-			if (!url.contains(domain)) {
-				int offset = requestURI.indexOf("/", requestURI.indexOf("://") + 3);
-				requestURI.insert(offset, "." + domain);
-			}
-			if (!cookie.isEmpty()) {
-				requestHeaders.add(COOKIE_HEADER, cookie);
-			}
-		}
+    if (this.session != null) {
+      String url = requestURI.toString();
+      String domain = this.session.getDomain();
+      String cookie = this.session.getCookie();
+      if (!url.contains(domain)) {
+        int offset = requestURI.indexOf("/", requestURI.indexOf("://") + 3);
+        requestURI.insert(offset, "." + domain);
+      }
+      if (!cookie.isEmpty()) {
+        requestHeaders.add(COOKIE_HEADER, cookie);
+      }
+    }
 
-		System.out.println(requestURI.toString());
-		System.out.println(">>> " + requestHeaders.entrySet());
-	}
+    System.out.println(requestURI.toString());
+    System.out.println(">>> " + requestHeaders.entrySet());
+  }
 
-	@Override
-	public void rewriteResponse(StringBuilder requestMethod, StringBuilder requestURI, Headers requestHeaders, Headers responseHeaders) throws IOException {
+  @Override
+  public void rewriteResponse(StringBuilder requestMethod, StringBuilder requestURI,
+      Headers requestHeaders, Headers responseHeaders) throws IOException {
 
-		// DIRTY HACK: Rewrite Content-type to Content-Type
-		if (responseHeaders.containsKey(CONTENT_TYPE_HEADER_LOWER)) {
-			List<String> contentTypes = responseHeaders.remove(CONTENT_TYPE_HEADER_LOWER);
-			Map<String, List<String>> contentHeaders = new HashMap<String, List<String>>();
+    // DIRTY HACK: Rewrite Content-type to Content-Type
+    if (responseHeaders.containsKey(CONTENT_TYPE_HEADER_LOWER)) {
+      List<String> contentTypes = responseHeaders.remove(CONTENT_TYPE_HEADER_LOWER);
+      Map<String, List<String>> contentHeaders = new HashMap<String, List<String>>();
 
-			contentHeaders.put(CONTENT_TYPE_HEADER_UPPER, contentTypes);
-			responseHeaders.putAll(contentHeaders);
-		}
+      contentHeaders.put(CONTENT_TYPE_HEADER_UPPER, contentTypes);
+      responseHeaders.putAll(contentHeaders);
+    }
 
-		System.out.println("<<< " + responseHeaders.entrySet());
-	}
+    System.out.println("<<< " + responseHeaders.entrySet());
+  }
 }
